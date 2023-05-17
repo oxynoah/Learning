26d0838abf988291f6c2b1f2c80296f0f6c0b40a
oxynoah <oxynoahb@gmail.com>
2023-05-17 08:22:33 -0700

    loops and control flow

--- Learning/Learning.cpp
+++ Learning/Learning.cpp
@@ -1,4 +1,15 @@
+#include "Header File.hpp"
+
 void main()
 {
+	// Loops
+	for (int i = 0; i < 5; i++) { continue; break; }
+	int i;
+	while (i < 5)
+		i++;
 
+	// Control Flow
+	if (i > 1) {}
+	else if (i > 2) {}
+	else {}
 }
